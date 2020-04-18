a12911acc74379ca0411570055e18c388b1bd22e
denisqq <frrait98@gmail.com>
2020-04-18 23:55:13 +0300

    add lst file

--- AvrBuild.bat
+++ AvrBuild.bat
@@ -1,2 +1,2 @@
 @ECHO OFF
-"C:\Program Files (x86)\Atmel\AVR Tools\AvrAssembler2\avrasm2.exe" -S "C:\Users\denisqq\Documents\ms\labels.tmp" -fI -W+ie -C V2E -o "C:\Users\denisqq\Documents\ms\ms.hex" -d "C:\Users\denisqq\Documents\ms\ms.obj" -e "C:\Users\denisqq\Documents\ms\ms.eep" -m "C:\Users\denisqq\Documents\ms\ms.map" "C:\Users\denisqq\Documents\ms\ms.asm"
+"C:\Program Files (x86)\Atmel\AVR Tools\AvrAssembler2\avrasm2.exe" -S "C:\Users\denisqq\Documents\ms\labels.tmp" -fI -W+ie -C V2E -o "C:\Users\denisqq\Documents\ms\ms.hex" -d "C:\Users\denisqq\Documents\ms\ms.obj" -e "C:\Users\denisqq\Documents\ms\ms.eep" -m "C:\Users\denisqq\Documents\ms\ms.map" -l "C:\Users\denisqq\Documents\ms\ms.lst" "C:\Users\denisqq\Documents\ms\ms.asm"
